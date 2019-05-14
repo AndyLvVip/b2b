c26542721d42a70c613adeab5ded1043c07fb667
andy.lv <andy.lv.vip@hotmail.com>
2019-05-14 23:59:18 +0800

    fetch permission list by userId...

--- platform-sys/fs-platform-sys/src/main/java/uca/platform/sys/repository/UserRoleRepository.java
+++ platform-sys/fs-platform-sys/src/main/java/uca/platform/sys/repository/UserRoleRepository.java
@@ -1,6 +1,17 @@
 package uca.platform.sys.repository;
 
+import jooq.generated.platform.sys.tables.records.UserRoleRecord;
+import org.jooq.Configuration;
+import org.jooq.DSLContext;
 import org.springframework.stereotype.Repository;
+import uca.common.repository.StdStrRepository;
+import uca.platform.sys.domain.Permission;
+import uca.platform.sys.domain.UserRole;
+
+import java.util.List;
+
+import static jooq.generated.platform.sys.Tables.PERMISSION;
+import static jooq.generated.platform.sys.Tables.USER_ROLE;
 
 /**
  * Description:
@@ -9,5 +20,20 @@ import org.springframework.stereotype.Repository;
  * @date 2019/5/14 20:13
  */
 @Repository
-public class UserRoleRepository {
+public class UserRoleRepository extends StdStrRepository<UserRoleRecord, UserRole> {
+
+    private final DSLContext dsl;
+
+    public UserRoleRepository(Configuration configuration, DSLContext dsl) {
+        super(USER_ROLE, UserRole.class, configuration);
+        this.dsl = dsl;
+    }
+
+
+    public List<Permission> fetchAllPermissionList(String userId) {
+        return dsl.select(PERMISSION.fields())
+                .from(USER_ROLE).join(PERMISSION).on(USER_ROLE.ROLE_ID.eq(PERMISSION.ROLE_ID))
+                .where(USER_ROLE.USER_ID.eq(userId))
+                .fetchInto(Permission.class);
+    }
 }
